904fb0d9a21c6ecde8155fdc9895c3f1cc032bb3
Camille-Li-1022 <lmengya1022@gmail.com>
2025-01-13 18:36:07 +0800

    env update
    1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+// 加载 .env 文件中的环境变量
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000
+  await app.listen(port);
+  console.log(`Server listen at: ${port}`)
 }
 bootstrap();
